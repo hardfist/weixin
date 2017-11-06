e294fa995c863b94dda6f817c5419e88e88de13d
yangjian.fe <yangjian.fe@bytedance.com>
2017-11-06 08:50:43 +0800

    接口配置成功
    Change-Id: I5c6129113e47903866597fa51c6b77a1eb654a83

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-const Koa = require('koa');
+import Koa from 'koa';
+import fs from 'fs';
+import sha1 from 'sha1';
+
 const app = new Koa();
+const config = JSON.parse(fs.readFileSync('./config/conf.local.json'));
 
 // response
-app.use(ctx => {
-  ctx.body = 'Hello Yj';
-});
-
+app.use(function* (next) {
+  console.log(this.query);
+  const token = config.wechat.token;
+  const { signature, nonce, timestamp, echostr} = this.query;
+  console.log('token',token);
+  const str = [token, timestamp, nonce].sort().join('');
+  const sha = sha1(str);
+  if(sha == signature){
+    this.body = echostr+ ''
+  }else { 
+    this.body = 'wrong'
+  }
+})
 app.listen(8080);
